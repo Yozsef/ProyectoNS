2d7856f0b08a916f051d6197197036bcfe0118fb
Esteban0022 <165233561+Esteban0022@users.noreply.github.com>
2024-04-11 21:55:01 -0600

    Agregar header
    ad

---
+++ webroot/src/cliente/js/Header.js
@@ -0,0 +1,22 @@
+var htmlSuperior = `<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="UTF-8" />
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	  <link rel="stylesheet" href="/css/estilo.css" />
+	  <script src="/js/funciones.js"></script>
+	  <title>Ventas App</title>
+      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+	</head>
+
+	<body>
+
+    <nav>
+      <img src="../../../dist/imagenes/NovaScript_logo.png">
+      <button> <a href="">Salir</a> </button>
+      <button><a href="Catalogo.html">Catalogo</a></button>
+    </nav>
+`;
+
+document.write(htmlSuperior);
