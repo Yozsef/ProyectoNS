e128d1fda01ecf5e4827b1ef1a02f367302985df
Jose David <jdavidgh30@gmail.com>
2024-04-18 10:55:01 -0600

    Footer Agregador

---
+++ webroot/src/cliente/js/footer.js
@@ -0,0 +1,45 @@
+var htmlInferior = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/footer.css" />
+</head>
+
+<body>
+   <footer class="footer">
+        <div class="container">
+
+            <div class="footer-row">
+
+                <div class="footer-links">
+                    <h4>Compañia</h4>
+                    <ul>
+                        <li><a href="">Nosotros</a></li>
+                        <li><a href="">Nuestros servicios</a></li>
+                        <li><a href="">Afiliate</a></li>
+                    </ul>
+                </div>
+                <div class="footer-desc">
+                    <h2>NovaScript</h2>
+                    <p>En NovaScript, nos dedicamos a trascender los límites de la creatividad y la innovación en el mundo digital. Somos más que una empresa de desarrollo de páginas web; somos artesanos digitales que tejen líneas de código para dar vida a tus visiones. Desde la creación de plataformas web elegantes hasta la implementación de soluciones tecnológicas vanguardistas, en NovaScript estamos comprometidos a impulsar la transformación digital de tu negocio.</p>
+                </div>
+                <div class="footer-links">
+                    <h4>Ayuda</h4>
+                    <ul>
+                        <li><a href="">Preguntas</a></li>
+                        <li><a href="">Compras</a></li>
+                        <li><a href="">Envios</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
+`;
+
+document.write(htmlInferior);
